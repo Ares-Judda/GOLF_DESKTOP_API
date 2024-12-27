0db57bf568260a66e461012cc54f5044761cca5e
LuisElizaldeX <king_mbea@hotmail.com>
2024-12-26 18:54:11 -0600

    Funcionalidades de articulas
    Las siguientes páginas cuentas con las funcionalidades correspondientes a los articulos:
    ClothesPage
    EarningsPage
    SellClothesPage
    HomePage
    ClothesDetails
    UpdateClothes
    
    Esto mediante la agregacion de servicios en ApiServiceGrpc

--- Views/Pages/ClothesPage.xaml.cs
+++ Views/Pages/ClothesPage.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using GOLF_DESKTOP.Model.Entities;
+using GOLF_DESKTOP.Model.Utilities;
+using GOLF_DESKTOP.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +22,91 @@ namespace GOLF_DESKTOP.Views.Pages {
     /// Lógica de interacción para ClothesPage.xaml
     /// </summary>
     public partial class ClothesPage : Page {
+        public ObservableCollection<Clothe> Clothes { get; set; }
+
         public ClothesPage() {
             InitializeComponent();
+            cargarArticulos();
         }
+
+        private async void MouseDownSearch(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                string articleName = txbSearchArticle.Text.Trim();
+
+                if (string.IsNullOrWhiteSpace(articleName))
+                {
+                    MessageBox.Show("Por favor, ingrese un nombre de artículo para buscar.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+
+                var articles = await ArticulosServiceGrpc.GetArticleByNameAsync(articleName);
+
+                if (articles.Any())
+                {
+                    listaArticulosVendedor.ItemsSource = articles;
+                }
+                else
+                {
+                    MessageBox.Show("No se encontraron artículos con un nombre similar.", "Información", MessageBoxButton.OK, MessageBoxImage.Information);
+                    listaArticulosVendedor.ItemsSource = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocurrió un error al buscar el artículo: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private async void CategorySelection(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                ComboBoxItem selectedItem = cbxCategory.SelectedItem as ComboBoxItem;
+                string category = selectedItem?.Content.ToString();
+
+                var articles = await ArticulosServiceGrpc.GetArticleByCategoryAsync(category);
+
+                if (articles.Any())
+                {
+                    listaArticulosVendedor.ItemsSource = articles;
+                }
+                else
+                {
+                    MessageBox.Show("No se encontraron artículos con esa categoría.", "Información", MessageBoxButton.OK, MessageBoxImage.Information);
+                    listaArticulosVendedor.ItemsSource = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocurrió un error al buscar el artículo: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+        private async void cargarArticulos()
+        {
+            var articulos = await ArticulosServiceGrpc.GetArticulosBySellingAsync(UserSingleton.GetInstance().IdUser);
+
+            Clothes = new ObservableCollection<Clothe>(articulos);
+
+            if (Clothes.Any())
+            {
+                listaArticulosVendedor.ItemsSource = Clothes;
+            }
+
+        }
+
+        private void MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var selectedClothe = (Clothe)listaArticulosVendedor.SelectedItem;
+
+            if (selectedClothe != null)
+            {
+                NavigationService.Navigate(new UpdateClothes(selectedClothe));
+            }
+        }
+
     }
 }
--- Views/Pages/HomePage.xaml.cs
+++ Views/Pages/HomePage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using GOLF_DESKTOP.Views.Windows;
+﻿using GOLF_DESKTOP.Model.Entities;
+using GOLF_DESKTOP.Services;
+using GOLF_DESKTOP.Views.Windows;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,12 +22,94 @@ namespace GOLF_DESKTOP.Views.Pages {
     /// Lógica de interacción para HomePage.xaml
     /// </summary>
     public partial class HomePage : Page {
+        public ObservableCollection<Clothe> Clothes { get; set; }
+
         public HomePage() {
             InitializeComponent();
+            getArticles();
         }
 
         private void MouseDownShopingCar(object sender, MouseButtonEventArgs e) {
             NavigationService.Navigate(new ShopingCarPage());
         }
+
+        private async void MouseDownSearch(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                string articleName = txbSearchArticle.Text.Trim();
+
+                if (string.IsNullOrWhiteSpace(articleName))
+                {
+                    MessageBox.Show("Por favor, ingrese un nombre de artículo para buscar.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+
+                var articles = await ArticulosServiceGrpc.GetArticleByNameAsync(articleName);
+
+                if (articles.Any())
+                {
+                    listaArticulos.ItemsSource = articles; 
+                }
+                else
+                {
+                    MessageBox.Show("No se encontraron artículos con un nombre similar.", "Información", MessageBoxButton.OK, MessageBoxImage.Information);
+                    listaArticulos.ItemsSource = null; 
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocurrió un error al buscar el artículo: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private async void CategorySelection(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                ComboBoxItem selectedItem = cbxCategory.SelectedItem as ComboBoxItem;
+                string category = selectedItem?.Content.ToString();
+
+                var articles = await ArticulosServiceGrpc.GetArticleByCategoryAsync(category);
+
+                if (articles.Any())
+                {
+                    listaArticulos.ItemsSource = articles;
+                }
+                else
+                {
+                    MessageBox.Show("No se encontraron artículos con esa categoría.", "Información", MessageBoxButton.OK, MessageBoxImage.Information);
+                    listaArticulos.ItemsSource = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocurrió un error al buscar el artículo: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private async void getArticles()
+        {
+            var articles = await ArticulosServiceGrpc.GetAllArticulosAsync();
+
+            Clothes = new ObservableCollection<Clothe>(articles);
+
+            if (Clothes.Any())
+            {
+                listaArticulos.ItemsSource = Clothes;
+            }
+
+        }
+
+        private void MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var selectedClothe = (Clothe)listaArticulos.SelectedItem;
+
+            if (selectedClothe != null)
+            {
+                NavigationService.Navigate(new ClothesDetails(selectedClothe));
+            }
+        }
+
     }
 }
